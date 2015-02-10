f832b13093e629712fb2f6931684e9c8b7f45d3f
Eduardo Poleo <eduardopoleo@gmail.com>
2015-02-09 22:51:11 -0500

    1st new lay out for blog

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,5 @@ Rails.application.routes.draw do
     resources :comments
   end
   
-  root 'welcome#index'
-
-#Prefix Verb   URI Pattern                  Controller#Action
-
- #   articles GET    /articles(.:format)          articles#index
- #            POST   /articles(.:format)          articles#create
- #new_article GET    /articles/new(.:format)      articles#new
-#edit_article GET    /articles/:id/edit(.:format) articles#edit
-   #  article GET    /articles/:id(.:format)      articles#show
-  #           PATCH  /articles/:id(.:format)      articles#update
-   #          PUT    /articles/:id(.:format)      articles#update
-     #        DELETE /articles/:id(.:format)      articles#destroy
-     #   root GET    /         
-  
+  root 'welcome#front_page'
 end
