311942958f089c5e38cf9b196e901e626ec0a850
Eduardo Poleo <eduardopoleo@gmail.com>
2015-05-04 19:27:48 -0400

    Add influitive cover letter

--- app/controllers/cover_letters_controller.rb
+++ app/controllers/cover_letters_controller.rb
@@ -1,5 +1,5 @@
 class CoverLettersController < ApplicationController
   def the_score; end
-
   def stack_builders; end
+  def influitive; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   
   root 'welcome#front_page'
   get 'welcome', to: 'welcome#front_page', as: 'welcome'
+  #Cover_letters
   get 'the_score', to: 'cover_letters#the_score', as: 'the_score'
   get 'stack_builders', to: 'cover_letters#stack_builders', as: 'stack_builders'
+  get 'influitive', to: 'cover_letters#influitive', as: 'influitive'
 end
 
