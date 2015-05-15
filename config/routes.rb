948cb73d5e68e8860f5f5fc0eabfd96b53624845
Eduardo Poleo <eduardopoleo@gmail.com>
2015-05-15 11:38:48 -0400

    Remove cl

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   root 'welcome#front_page'
   get 'welcome', to: 'welcome#front_page', as: 'welcome'
   #Cover_letters
-  get 'the_score', to: 'cover_letters#the_score', as: 'the_score'
-  get 'stack_builders', to: 'cover_letters#stack_builders', as: 'stack_builders'
   get 'influitive', to: 'cover_letters#influitive', as: 'influitive'
   get 'info_tech', to: 'cover_letters#info_tech', as: 'info_tech'
 end
